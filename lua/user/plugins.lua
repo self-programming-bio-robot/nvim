20a6ff2e6fd33f331ea1b6f2b01cee6701a7fba7
Zhdanov Nikolai <nikolai@zhdanov.space>
2023-03-21 10:00:46 +0300

    added rust supporting

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -22,4 +22,27 @@ return require('packer').startup(function()
 -- snippets
   use 'dcampos/nvim-snippy'
   use 'honza/vim-snippets'
+
+-- rust
+  use 'williamboman/mason.nvim'    
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig' 
+  use 'simrat39/rust-tools.nvim'
+
+  -- Completion framework:
+  use 'hrsh7th/nvim-cmp' 
+
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'                             
+  use 'hrsh7th/cmp-path'                              
+  use 'hrsh7th/cmp-buffer'                            
+  use 'hrsh7th/vim-vsnip' 
+  
+  -- terminal
+  use 'voldikss/vim-floaterm'
 end)
